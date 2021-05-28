eec838cbc53e496ee37bccc45eea89bc19b1c8b4
bachirelkhadir <bachir009+git@gmail.com>
2021-05-28 00:19:56 -0400

    solution1.py

--- solution1.py
+++ solution1.py
@@ -124,8 +124,8 @@ class SolutionOne(Scene):
         five_sixth = five_sixth.copy()
         five_sixth_2 = five_sixth.copy()
         equal.next_to(probs[2], RIGHT)
-        one_sixth.next_to(five_sixth, RIGHT, MED_LARGE_BUFF)
-        five_sixth.next_to(equal, RIGHT)
+        one_sixth.next_to(equal_sixth, RIGHT)
+        five_sixth.next_to(one_sixth, RIGHT, MED_LARGE_BUFF)
         five_sixth_2.next_to(five_sixth, RIGHT)
         rhs_obj.extend([equal, one_sixth, five_sixth, five_sixth_2])
 
