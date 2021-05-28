bd1092553197ae2637cfa0b772e8086d83956d9e
bachirelkhadir <bachir009+git@gmail.com>
2021-05-27 23:57:21 -0400

    solution1.py

--- solution1.py
+++ solution1.py
@@ -121,58 +121,58 @@ class SolutionOne(Scene):
         # P(T = 3)
         equal = equal.copy()
         five_sixth = five_sixth.copy()
-        onefive_sixth = onefive_sixth.copy()
-        onefive_sixthfive_2 = onefive_sixth.copy()
-        equal.nextfive_to(probs[2], RIGHT)
-        five_sixth.nextfive_to(equal, RIGHT)
-        onefive_sixth.nextfive_to(five_sixth, RIGHT, MEDfive_LARGEfive_BUFF)
-        onefive_sixthfive_2.nextfive_to(onefive_sixth, RIGHT)
-        rhsfive_obj.extend([equal, five_sixth, onefive_sixth, onefive_sixthfive_2])
+        one_sixth = one_sixth.copy()
+        one_sixth_2 = one_sixth.copy()
+        equal.next_to(probs[2], RIGHT)
+        five_sixth.next_to(equal, RIGHT)
+        one_sixth.next_to(five_sixth, RIGHT, MED_LARGE_BUFF)
+        one_sixth_2.next_to(one_sixth, RIGHT)
+        rhs_obj.extend([equal, five_sixth, one_sixth, one_sixth_2])
 
 
-        self.play(ShowCreation(arrfive_Tfive_numbers[2]))
+        self.play(ShowCreation(arr_T_numbers[2]))
         self.add(probs[2])
         self.wait()
         self.add(equal)
         self.add(five_sixth)
         self.wait()
-        self.add(onefive_sixth)
+        self.add(one_sixth)
         self.wait()
-        self.add(onefive_sixthfive_2)
+        self.add(one_sixth_2)
         self.wait()
 
         # 5/6 5/6 to (5/6)^2
-        parenfive_powfive_2 = Tex(r"\left({{\frac 1 6}}\right)^{2}").scale(TEXTfive_SCALE)
-        parenfive_powfive_2 = VGroup(parenfive_powfive_2[0], parenfive_powfive_2[2])
-        parenfive_powfive_2.shift(onefive_sixth.getfive_center() -\
-            (parenfive_powfive_2[0].getfive_corner(RIGHT)+parenfive_powfive_2[1].getfive_corner(LEFT))/2
+        paren_pow_2 = Tex(r"\left({{\frac 1 6}}\right)^{2}").scale(TEXT_SCALE)
+        paren_pow_2 = VGroup(paren_pow_2[0], paren_pow_2[2])
+        paren_pow_2.shift(one_sixth.get_center() -\
+            (paren_pow_2[0].get_corner(RIGHT)+paren_pow_2[1].get_corner(LEFT))/2
             )
 
 
-        self.play(Transform(onefive_sixthfive_2, parenfive_powfive_2))
+        self.play(Transform(one_sixth_2, paren_pow_2))
         self.wait()
 
         # P(T = k)
         equal = equal.copy()
         five_sixth = five_sixth.copy()
-        onefive_sixthfive_k = Tex(r"\left(\frac 5 6\right)^{\phantom{k}-1}").scale(TEXTfive_SCALE)
-        onefive_sixthfive_kfive_power = Tex(r"k", color=YELLOW).scale(.75).scale(TEXTfive_SCALE)
-        onefive_sixthfive_kfive_power.movefive_to(onefive_sixthfive_k.getfive_corner(UP)).shift(.09*DOWN+.22*RIGHT)
-        onefive_sixthfive_k = VGroup(onefive_sixthfive_k, onefive_sixthfive_kfive_power)
+        one_sixth_k = Tex(r"\left(\frac 5 6\right)^{\phantom{k}-1}").scale(TEXT_SCALE)
+        one_sixth_k_power = Tex(r"k", color=YELLOW).scale(.75).scale(TEXT_SCALE)
+        one_sixth_k_power.move_to(one_sixth_k.get_corner(UP)).shift(.09*DOWN+.22*RIGHT)
+        one_sixth_k = VGroup(one_sixth_k, one_sixth_k_power)
 
-        equal.nextfive_to(probs[-1], RIGHT)
-        five_sixth.nextfive_to(equal, RIGHT)
-        onefive_sixthfive_k.nextfive_to(five_sixth, RIGHT, MEDfive_SMALLfive_BUFF)
-        rhsfive_obj.extend([equal, five_sixth, onefive_sixthfive_k])
-        probfive_Tfive_k = VGroup(five_sixth, onefive_sixthfive_k) # keep track of this bc we use it below
+        equal.next_to(probs[-1], RIGHT)
+        five_sixth.next_to(equal, RIGHT)
+        one_sixth_k.next_to(five_sixth, RIGHT, MED_SMALL_BUFF)
+        rhs_obj.extend([equal, five_sixth, one_sixth_k])
+        prob_T_k = VGroup(five_sixth, one_sixth_k) # keep track of this bc we use it below
 
-        self.play(ShowCreation(VGroup(*arrfive_Tfive_numbers[3:])))
+        self.play(ShowCreation(VGroup(*arr_T_numbers[3:])))
 
         self.add(*probs[1:])
         self.wait()
         self.add(equal)
         self.add(five_sixth)
-        self.add(onefive_sixthfive_k)
+        self.add(one_sixth_k)
         self.wait()
         return
 
