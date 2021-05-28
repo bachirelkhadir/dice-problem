0a2a955f4ba86fc61a5e70e2b0dc4c02d6a86f47
bachirelkhadir <bachir009+git@gmail.com>
2021-05-28 00:01:20 -0400

    solution1.py

--- solution1.py
+++ solution1.py
@@ -248,7 +248,6 @@ class SolutionOne(Scene):
         # P(T = k) ==> 1/6 5/6^k
         prob_T_k.move_to(generic_term[1]).shift(RIGHT/10.)
         self.play(Transform(generic_term[1], prob_T_k))
-        generic_term[1] = prob_T_k
         self.wait()
 
         # take 1/6 out
