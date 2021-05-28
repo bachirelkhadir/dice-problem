0fb4b1748a11fbf11b5d129c9825be8011162a89
bachirelkhadir <bachir009+git@gmail.com>
2021-05-28 00:09:24 -0400

    solution1.py

--- solution1.py
+++ solution1.py
@@ -391,4 +391,4 @@ class SolutionOne(Scene):
         self.add(frame)
 
         self.play(six_with_rect.animate.next_to(var_T, RIGHT, LARGE_BUFF),
-                  frame.animate.set_height(8))
+                  frame.animate.set_height(2))
