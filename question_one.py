81b047222a1c907b0055ca1f8ef96616a2ba28c8
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 13:35:59 -0400

    question_one.py

--- question_one.py
+++ question_one.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class QuestionOne(Scene):
     def construct(self):
-        np.random
+        np.random.seed(0)
         game = np.random.randint(1, 7, 1000)
         game = "".join(map(str, game))
         i = game.find("66")
