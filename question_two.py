08d633de081a4099661675547e3f644ca1725079
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 14:06:44 -0400

    question_two.py

---
+++ question_two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+from manimlib import *
+exec(get_custom_config()["universal_import_line"])
+
+import numpy as np
+from dice_utils import make_dice_face
+from common import vstack, hstack, halign
+
+def hstack_fixed_width(obj, width, buff):
+    vstack(obj[::width], buff)
+
+    for i in range(len(obj) // width):
+        hstack(obj[i*width:(i+1)*width], buff)
+
+    hstack(obj[(i+1)*width:], buff)
+
+class QuestionTwo(Scene):
+    def construct(self):
+
+        frame = self.camera.frame
+
+        np.random.seed(0)
+        games = np.random.randint(1, 7, 1000)# burn in to make seq nice
