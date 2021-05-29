9c57a67eed6f2e4712241df6ecbc597eea1352bc
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 14:15:19 -0400

    question_two.py

--- question_two.py
+++ question_two.py
@@ -17,7 +17,7 @@ def hstack_fixed_width(obj, width, buff):
     hstack(obj[(i+1)*width:], buff)
 
 def string_to_dice(game):
-    return VGroup(*hstack([*map(lambda s: make_dice_face(int(i)), game)]))
+    return VGroup(*hstack([*map(lambda s: make_dice_face(int(s)), game)]))
 
 class QuestionTwo(Scene):
 
