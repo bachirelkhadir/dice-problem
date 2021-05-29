65db40cb3d92843ea357947f1f00e2bab1bfc538
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 13:35:28 -0400

    question_one.py

--- question_one.py
+++ question_one.py
@@ -10,6 +10,7 @@ class QuestionOne(Scene):
         game = np.random.randint(1, 7, 1000)
         game = "".join(map(str, game))
         i = game.find("66")
-        game = game[:i+2]
+        game = [*map(int, game[:i+2])]
+        first_six = game.find("6")
         print(game)
         self.add(make_dice_face(1))
