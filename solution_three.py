8bb3f3a42216c809a13cd2a521843c24df5158ce
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 00:42:30 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -218,9 +218,11 @@ class SolutionThree(Scene):
         highlight_game(self, VGroup(*dice[5]))
         self.wait()
 
+
+        num_games = game_counter[1].copy()
         num_6 = Text(r"= # 6s").scale(tex_scale)
         one_sixth = Tex(r"\approx \frac 1 6").scale(tex_scale)
-        num_games = game_counter[1].copy()
+        num_throws = num_throws.copy()
 
         one_sixth.next_to(num_throws, DOWN).to_corner(RIGHT)
         num_games.next_to(one_sixth, RIGHT).shift(UP/20)
