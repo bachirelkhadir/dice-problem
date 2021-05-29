4f05e0c5692dee9ab47aa2f3c17de5f699f9c53e
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 13:26:36 -0400

    question_one.py

--- question_one.py
+++ question_one.py
@@ -2,7 +2,9 @@
 from manimlib import *
 exec(get_custom_config()["universal_import_line"])
 
+import numpy as np
 
 class QuestionOne(Scene):
     def construct(self):
+
         self.add(Tex("Q1"))
