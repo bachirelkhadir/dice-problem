7016fda3f05cb8feeb5dd8e2eff5cc586e3fe14a
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 14:17:18 -0400

    question_two.py

--- question_two.py
+++ question_two.py
@@ -33,13 +33,15 @@ class QuestionTwo(Scene):
         np.random.seed(1)
         games = np.random.randint(1, 7, 1000)# burn in to make seq nice
         games = "".join(map(str, games)).split("6")
-        even_games = [g+"6" for g in games if len(set(g) & set("135")) == 0]
-        odd_games = [g+"6" for g in games if len(set(g) & set("135")) != 0]
-        print("even:", even_games)
-        print("odd:", odd_games)
-
-        even_dice = [
-            string_to_dice(s) for s in even_games
+        dice = [
+            string_to_dice(s+"6") for s in games
         ]
-        vstack(even_dice)
+
+        vstack(dice)
+        even_games = [d for g, d in zip(games, dice)
+                      if len(set(g) & set("135")) == 0]
+
+        odd_games = [d for g, d in zip(games, dice)
+                      if len(set(g) & set("135")) == 0]
+
         self.add(*even_dice)
