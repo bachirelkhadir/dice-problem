52cce2fd899b9410d94c582aba2dd2ce5138dc49
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 13:39:02 -0400

    question_one.py

--- question_one.py
+++ question_one.py
@@ -9,7 +9,7 @@ from common import vstack, hstack, halign
 class QuestionOne(Scene):
     def construct(self):
         np.random.seed(0)
-        game = np.random.randint(1, 7, 1000)[7:] # burn in to make seq nice
+        game = np.random.randint(1, 7, 1000)[100:] # burn in to make seq nice
         game = "".join(map(str, game))
         first_66 = game.find("66")
         game = game[:first_66+2]
