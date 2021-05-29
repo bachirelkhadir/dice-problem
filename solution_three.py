ba221ce5c1e7af0b96398961cdf9c48a30760e1d
bachirelkhadir <bachir009+git@gmail.com>
2021-05-28 22:51:21 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -160,7 +160,7 @@ class SolutionThree(Scene):
         ET = Tex(r"\mathbb E[T]")
         num_games = game_counter[1].copy()
         ET.next_to(num_throws, DOWN).to_corner(LEFT)
-        num_game.next_to(ET, RIGHT)
+        num_games.next_to(ET, RIGHT)
         self.add(ET)
         self.wait()
 
