d5dc429d3eff1f3687e1ac223123d01e279adee4
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 01:19:54 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -160,7 +160,8 @@ class SolutionThree(Scene):
         # zoom out and add rest of the games
         frame = self.camera.frame
         frame_target = frame.copy().set_height(camera_height).align_to(dice[0], UP).shift(UP)
-        always(game_counter.align_to, frame, LEFT)
+        game_counter.add_updater(lambda m: m.align_to(frame, LEFT).shift(RIGHT))
+        #always(game_counter.align_to, frame, LEFT)
 
         total_time = 2
         game_counter[1].curr_time = 0
