7d9c3e417ba3a7e26e5c59faf71ca728ec9a761b
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 01:55:02 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -238,10 +238,13 @@ class SolutionThree(Scene):
 
         num_6 = Text(r"= #6s").scale(tex_scale)
         one_sixth = Tex(r"\approx {{\frac 1 6}}").scale(1.2*tex_scale)
+        one_sixth.move_to(num_6)
         one_sixth[1].set_color(COLOR3)
 
+
         approx2 = VGroup(num_games, num_6, one_sixth, num_throws)
-        hstack(approx2, MED_SMALL_BUFF)
+        #hstack(approx2, MED_SMALL_BUFF)
+
 
 
         # brin down 100
