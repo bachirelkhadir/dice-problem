a50497fbf12fa351140c9007de50b12859ea4794
bachirelkhadir <bachir009+git@gmail.com>
2021-05-28 21:45:13 -0400

    solution1.py

--- solution1.py
+++ solution1.py
@@ -13,6 +13,7 @@ def hstack(objs, buff=LARGE_BUFF):
     for t1, t2 in zip(objs, objs[1:]):
         t2.next_to(t1, RIGHT, buff)
     return objs
+
 def halign(objs):
     for t in objs[1:]:
         t.align_to(objs[0])
