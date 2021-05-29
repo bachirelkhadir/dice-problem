fba949f449bcecc8d62747c2f203019efcd7de0a
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 14:21:26 -0400

    question_two.py

--- question_two.py
+++ question_two.py
@@ -46,7 +46,7 @@ class QuestionTwo(Scene):
                       if len(set(g) & set("135")) == 0]
 
         odd_games = [d for g, d in zip(games, dice)
-                      if len(set(g) & set("135")) == 0]
+                      if len(set(g) & set("135")) 1= 0]
 
         self.play(ShowIncreasingSubsets(VGroup(*dice)))
         self.wait()
