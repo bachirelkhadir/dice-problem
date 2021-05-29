0d3d6b4df6c7af087034cec6fd7d76f5626b0eb1
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 00:15:12 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -63,7 +63,7 @@ def place_dice_in_matrix(dice_matrix):
 
 def highlight_game(scene, game):
         rect = Rectangle(fill_color=BLACK, fill_opacity=.7)
-        rect.surround(game, stretch=True).scale(1.2)
+        rect.surround(game, stretch=True).scale(1.2*UP)
         scene.add(rect)
         scene.bring_to_front(game)
 
