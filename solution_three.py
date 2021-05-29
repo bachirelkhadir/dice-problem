e6df454a7d480041dad4f48c9558baf926c857cb
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 00:12:08 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -65,6 +65,7 @@ def highlight_game(scene, game):
         rect = Rectangle(fill_color=BLACK, fill_opacity=.7)
         rect.surround(game, stretch=True).scale(1.1)
         game.add_to_back(rect)
+        self.add(rect)
 
 
 class SolutionThree(Scene):
