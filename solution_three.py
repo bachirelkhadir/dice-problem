3e4e0482af5fadfa9b21be98ede8999adf31d2ed
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 01:25:11 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -198,7 +198,7 @@ class SolutionThree(Scene):
         # Method 1
         # #################
         # num throws = E[T] * 100
-        tex_scale = 3
+        tex_scale = 1.5
         num_throws = Text("# throws").scale(tex_scale).set_color(COLOR2)
         num_throws.align_to(game_counter, LEFT).shift(5*DOWN)
         self.add(num_throws)
