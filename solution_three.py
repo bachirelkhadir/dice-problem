e4fcb0a8796b3fbb826e899e2d51162ba9a2e0f8
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 01:12:53 -0400

    solution_three.py

--- solution_three.py
+++ solution_three.py
@@ -194,7 +194,7 @@ class SolutionThree(Scene):
         # Method 1
         # #################
         # num throws = E[T] * 100
-        num_throws = Text("# throws")
+        num_throws = Text("# throws").set_color(COLOR2)
         num_throws.align_to(frame, DL).shift(3*UP)
         self.add(num_throws)
         self.wait()
