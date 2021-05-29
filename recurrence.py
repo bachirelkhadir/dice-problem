66c6fa3062563a8a430ca6d3b26d1dfb63cbada6
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 13:27:59 -0400

    recurrence.py

--- recurrence.py
+++ recurrence.py
@@ -6,22 +6,6 @@ COLOR1 = YELLOW
 COLOR2 = GOLD
 COLOR3 = RED_A
 
-def make_node(s, r=1.2):
-    if type(s) == str:
-        text = Text(s)
-    else:
-        text = s
-    rect = Circle().scale(r)
-    #rect.surround(text)
-    return VGroup(text, rect)
-
-def make_dice_face(i):
-    dots = {
-        6: VGroup(*[Dot().shift( (i-1) * DOWN/3 + (j-.5) * RIGHT/2).scale(1.1) for i in range(3) for j in range(2)])
-    }
-    sq = Square(1.2, fill_opacity=.8, fill_color=BLACK).round_corners(.25).move_to(dots[i])
-    face = VGroup(sq, dots[i])
-    return face
 
 
 class RecurrenceScene(Scene):
