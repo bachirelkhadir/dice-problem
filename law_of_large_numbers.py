e0c2084363f74165e1757b74bc61bf55f4baee6d
bachirelkhadir <bachir009+git@gmail.com>
2021-05-30 17:37:23 -0400

    law_of_large_numbers.py

--- law_of_large_numbers.py
+++ law_of_large_numbers.py
@@ -48,7 +48,7 @@ class LLNWithCoins(Scene):
 
         self.add(coin1)
 
-        coins = [Coin() for _ in range(6)]
+        coins = [Coin().scale(.3) for _ in range(6)]
         coins[0].to_edge(LEFT)
         hstack(coins)
         self.add(*coins)
