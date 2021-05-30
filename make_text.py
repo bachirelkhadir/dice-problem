4bc4b68023823647ff6483173d095e51166c8c8f
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 20:51:22 -0400

    make_text.py

--- make_text.py
+++ make_text.py
@@ -75,7 +75,7 @@ class Conditional(TextScene):
                 Text("every throw"),
                 Tex(r"\in"),
                 hstack(*[make_dice_face(i).scale(0.3) for i in (2,4,6)]),
-                ]
+                ],
                     MED_SMALL_BUFF)
         )
         text.to_edge(LEFT)
