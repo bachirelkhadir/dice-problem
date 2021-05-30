37a636257c77b28e1947a4dd9b14ad44d00dc921
bachirelkhadir <bachir009+git@gmail.com>
2021-05-30 13:50:20 -0400

    law_of_large_numbers.py

---
+++ law_of_large_numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+from manimlib import *
+exec(get_custom_config()["universal_import_line"])
+import numpy as np
+from dice_utils import make_dice_face
+from common import vstack, hstack, halign
+
+
+class LawLargeNumbers(Scene):
