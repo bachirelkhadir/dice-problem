f95fc27482e7dc0c02ef2a907d02074b7ae635ea
bachirelkhadir <bachir009+git@gmail.com>
2021-05-30 14:52:50 -0400

    law_of_large_numbers.py

--- law_of_large_numbers.py
+++ law_of_large_numbers.py
@@ -14,8 +14,10 @@ class LawLargeNumbers(Scene):
         X.shift(UP)
         self.add(X)
 
+        # X1, X2, X3, X4, ..., X_n
         Xis = [Tex("X_{%s}" % i) for i in [1, 2, 3, 4, "n"]]
         Xis.insert(-1, Tex("..."))
+        # centering
         VGroup(*hstack(Xis)).move_to(0)
 
         self.play(LaggedStart(*[TransformFromCopy(X, Xi) for Xi in Xis]))
