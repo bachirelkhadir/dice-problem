8a349c2ea40b550dffc7fae2cb3cd3df2f860f45
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 20:48:57 -0400

    make_text.py

--- make_text.py
+++ make_text.py
@@ -71,7 +71,7 @@ class Conditional(TextScene):
             Tex(r"\mathbb E[ T"),
             Tex(r" | "),
             make_dice_face(1),
-            Tex(" \in {2, 4, 6}")
+            Tex(r"\in {2, 4, 6}")
         )
         #text[2].shift(2*DOWN)
 
