a51ca72afe7ac00dc3e51bb1801c088060bd2820
bachirelkhadir <bachir009+git@gmail.com>
2021-05-29 21:47:30 -0400

    make_text.py

--- make_text.py
+++ make_text.py
@@ -87,7 +87,7 @@ class Conditional(TextScene):
                 ],
                 MED_SMALL_BUFF)
         )
-        commas = VGroup(text[3], text[5]).shift(DOWN)
+        commas = VGroup(text[6], text[5]).shift(DOWN)
 
         part1 = text[:2]
         part2 = text[2:4]
