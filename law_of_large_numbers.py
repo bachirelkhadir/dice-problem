4e5de3a4f1d30f5eb48d24f899029bb700896de0
bachirelkhadir <bachir009+git@gmail.com>
2021-05-30 19:21:47 -0400

    law_of_large_numbers.py

--- law_of_large_numbers.py
+++ law_of_large_numbers.py
@@ -58,7 +58,7 @@ class LLNWithCoins(Scene):
         print(f"heads: {len(heads)}({len(coins)})")
 
         # center
-        coins[0].to_edge(LEFT).shift(1.5*RIGHT).shift(DOWN)
+        coins[0].move_to( - len(coins)//2 * coin_template.get_width() * LEFT )
         hstack_fixed_width(coins, width, SMALL_BUFF)
 
 
