ea5b2824293e78408adbc5e7c9cb97b95cc283a6
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 14:15:39 -0400

    color_area.py

--- color_area.py
+++ color_area.py
@@ -32,7 +32,7 @@ class ColorArea(Scene):
         axes = Axes(x_range, (-.1, 1))
         axes.add_coordinate_labels()
 
-        self.play(Write(axes, lag_ratio=0.01, run_time=1))
+        # self.play(Write(axes, lag_ratio=0.01, run_time=1))
 
         graph = axes.get_graph(
             lambda x: np.exp(-x*x/2),
@@ -40,7 +40,6 @@ class ColorArea(Scene):
         )
         #label = axes.get_graph_label(graph, "\\sin(x)")
 
-        self.add(graph)
         self.play(
             ShowCreation(graph),
             #FadeIn(label, RIGHT),
