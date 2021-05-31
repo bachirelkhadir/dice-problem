5aee811f3e5acf90f222e82b478fb7c1752c4bb0
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 14:17:57 -0400

    equally_likely.py

--- equally_likely.py
+++ equally_likely.py
@@ -3,9 +3,14 @@
 #!/usr/bin/env python3
 
 from manimlib import *
+exec(get_custom_config()["universal_import_line"])
 import numpy as np
+from dice_utils import make_dice_face
 
 
 class EquallyLikely(Scene):
     def construct(self):
-        pass
+        dice =[
+            make_dice_face(i) for i in range(1, 7)
+        ]
+        self.add(*dice)
