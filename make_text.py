5ea618ebd4b489b2924277471a244668d22b061e
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 17:39:57 -0400

    make_text.py

--- make_text.py
+++ make_text.py
@@ -109,7 +109,7 @@ class Sumkxk(TextScene):
         summand = Tex(r" {{k}} \ x^{k-1} = \frac 1 {(1-x)^2}", tex_to_color_map={"k": YELLOW})
         hstack([sigma, summand], MED_SMALL_BUFF)
 
-        self.play(ShowCreation(sigma, summand))
+        self.play(ShowCreation(VGroup(sigma, summand)))
 
     
 class Current(ThirdWayQuestion):
