ef8d2b5d995ab7ee11a474345d43f4cf04a95964
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 13:53:54 -0400

    color_area.py

--- color_area.py
+++ color_area.py
@@ -13,26 +13,13 @@ class ColorArea(Scene):
 
         self.play(Write(axes, lag_ratio=0.01, run_time=1))
 
-        # Axes.get_graph will return the graph of a function
         sin_graph = axes.get_graph(
-            lambda x: 2 * math.sin(x),
+            lambda x: np.exp(-x*x/2)
             color=BLUE,
         )
         # By default, it draws it so as to somewhat smoothly interpolate
         # between sampled points (x, f(x)).  If the graph is meant to have
         # a corner, though, you can set use_smoothing to False
-        relu_graph = axes.get_graph(
-            lambda x: max(x, 0),
-            use_smoothing=False,
-            color=YELLOW,
-        )
-        # For discontinuous functions, you can specify the point of
-        # discontinuity so that it does not try to draw over the gap.
-        step_graph = axes.get_graph(
-            lambda x: 2.0 if x > 3 else 1.0,
-            discontinuities=[3],
-            color=GREEN,
-        )
 
         # Axes.get_graph_label takes in either a string or a mobject.
         # If it's a string, it treats it as a LaTeX expression.  By default
