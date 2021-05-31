43f75b334a4878d9b02076c03aae688fe38d4a23
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 16:15:06 -0400

    equally_likely.py

--- equally_likely.py
+++ equally_likely.py
@@ -30,14 +30,18 @@ class Not6Then6(Scene):
     CONFIG = {
         "stroke_color": YELLOW_D,
         "stroke_width": 10,
-
-
     }
 
     def construct(self):
         dice1 = make_dice_face(6)
         dice2 = dice1.copy()
+        hstack([dice1, dice2])
         not_six = VGroup(Cross(dice1,), dice1)
         not_six[0].set_stroke(self.stroke_color, self.stroke_width)
+
+        self.add(dice1)
+        self.play(FadeIn(not_six[0]))
+        self.wait()
+
         self.add(dice2)
         self.play(FadeIn(not_six[0]))
