6094521286e723a7f141567daa890a9cd9be2b28
bachirelkhadir <bachir009+git@gmail.com>
2021-05-31 20:40:42 -0400

    slider.py

--- slider.py
+++ slider.py
@@ -84,7 +84,7 @@ class BouncingSolution(Slider):
         three_sol = Text("3 Solutions", t2c=t2c).scale(SCALE).to_corner(UP)
 
         sol = three_sol[1:-1]
-        self.add(sol)
+        self.add(three_sol)
         self.wait()
 
         dt = 1. / 60
