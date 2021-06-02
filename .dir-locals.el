3abc2f9b41fe7f3df344c5d9f1c924d8b01455d1
bachirelkhadir <bachir009+git@gmail.com>
2021-06-01 20:57:48 -0400

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,4 +4,5 @@
 ((python-mode . ((pyvenv-workon . "manim")
                  (eval . (bachir-prog-local-mode 1))
                  (eval . (git-auto-commit-mode 1))
-                 (eval . (message  "Local dir variables exectued for python mode.")))))
+                 (eval . (message  "Local dir variables exectued for python mode."))))
+ (nil  . ((eval . (git-auto-commit-mode 1)))))
